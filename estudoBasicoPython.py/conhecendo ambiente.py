11b025446730212207c96efd83880cdea50e0a03
GabrielMinamoto <gabrielminamoto@gmail.com>
2026-02-26 18:42:26 -0300

    conhecendo ambiente da linguagem

---
+++ estudoBasicoPython.py/conhecendo ambiente.py
@@ -0,0 +1,20 @@
+#começando a Aula de Python
+print(5)
+#sempre usar aspas para escrever
+print('Olá pessoal')
+#só posso juntar texto com texto, ou número com número
+print(6+9)
+#variavel
+produto1 = 5
+print(produto1)
+produto2 = 2000
+produto3 = 1
+print(produto1 + produto2 + produto3)
+#nomes de variaveis
+nomeProduto1 = "Macarrão"
+nomeProduto2 = "Celular"
+nomeProduto3 = "bala"
+#nomes colados
+print( nomeProduto1 + nomeProduto2 + nomeProduto3)
+#separando os nomes
+print( nomeProduto1 + " " + nomeProduto2 + " " +nomeProduto3)
