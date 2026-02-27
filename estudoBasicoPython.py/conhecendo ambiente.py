15b08627ab3e3cc0667c0f1a8f187dc5e2d6e935
GabrielMinamoto <gabrielminamoto@gmail.com>
2026-02-27 17:04:46 -0300

    tipos de dados e como descobri-los

--- estudoBasicoPython.py/conhecendo ambiente.py
+++ estudoBasicoPython.py/conhecendo ambiente.py
@@ -2,22 +2,28 @@
 print(5)
 #sempre usar aspas para escrever
 print('Olá pessoal')
+#-----------------------------
 #só posso juntar texto com texto, ou número com número
 print(6+9)
+#-----------------------------
 #variavel
 produto1 = 5
 print(produto1)
 produto2 = 2000
 produto3 = 1
 print(produto1 + produto2 + produto3)
+#-----------------------------
 #nomes de variaveis
 nomeProduto1 = "Macarrão"
 nomeProduto2 = "Celular"
 nomeProduto3 = "bala"
+#-----------------------------
 #nomes colados
 print( nomeProduto1 + nomeProduto2 + nomeProduto3)
+#-----------------------------
 #separando os nomes
 print( nomeProduto1 + " " + nomeProduto2 + " " +nomeProduto3)
+#-----------------------------
 #soma de variaveis
 amigos = 5
 aluguel = 1000
@@ -25,11 +31,13 @@ supermercado = 500
 carro = 400
 total = ( aluguel + supermercado + carro)
 print(total)
+#-----------------------------
 #subtração de variaveis
 aluguel = aluguel - 100
 print(aluguel)
 total = ( aluguel + supermercado + carro)
 print(total)
+#-----------------------------
 #multiplicação de variaveis
 amigos = 6
 aluguel = 1000
@@ -40,11 +48,22 @@ total_de_carros = carro * 2
 print(total_de_carros)
 total = aluguel + supermercado + total_de_carros
 print(total)
+#-----------------------------
 #divisão de variaveis
 total_por_pessoa = total / amigos
 print(total_por_pessoa)
+#-----------------------------
 #REGRAS DE NOMENCLATURA
 #1- Não pode começar com número
 #2- Não pode conter espaços
 #3- Não pode conter caracteres especiais
 #4- Não pode ser o nome de um comando do Python
+#-----------------------------
+#TIPOS DE DADOS
+#para descobrir tipo de dado -->
+type(aluguel)
+#tipos -->
+#int - números inteiros
+#float - números decimais
+#str - texto
+#bool - verdadeiro ou falso
